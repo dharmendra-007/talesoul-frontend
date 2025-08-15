e0f82633e7409a8baf8372dcfe8deef6c933a701
Dharmendra Mahanta <dharmendra-007@dharmendra.space>
2025-08-16 04:20:58 +0530

    feat(auth): add signup and signin pages with form validation

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -20,6 +20,8 @@ const buttonVariants = cva(
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
+        primaryButton: "bg-primary text-primary-foreground shadow-xs hover:bg-primary/90 bg-[#D96F62] text-[1rem] font-bold rounded-lg hover:bg-[#e07d70] cursor-pointer",
+        secondaryButton: "bg-primary text-[#3A3A3A] shadow-xs bg-white text-[1rem] rounded-lg mt-2 cursor-pointer",
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
