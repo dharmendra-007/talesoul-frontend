3a94b21bb8842baa9046db2dff4674cb780e012b
Gyan <hi.gyanaranjanpatra@gmail.com>
2025-08-16 19:36:56 +0530

    feat; navbar

---
+++ src/app/howitworks/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='h-screen w-screen bg-orange-400 flex items-center justify-center'>
+        <h1 className='text-white text-3xl'>How It Works page</h1>
+    </div>
+  )
+}
---
+++ src/components/ui/drawer/Drawer.tsx
@@ -0,0 +1,54 @@
+import { Button } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetFooter,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Menu } from "lucide-react";
+import Link from "next/link";
+
+interface NavLink {
+  linkText: string;
+  link: string;
+  type: string;
+}
+interface DrawerProps {
+  navLinks: NavLink[];
+}
+
+export default function Drawer({ navLinks }: DrawerProps) {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Menu />
+      </SheetTrigger>
+      <SheetContent className="pt-10 px-4">
+        <div className="flex flex-col gap-2 w-full">
+          {navLinks.map((link, index) => (
+            <Link href={link.link} key={index}>
+              <Button
+                variant="outline"
+                className={
+                  link.type === "link"
+                    ? "w-full"
+                    : "bg-[#F16D5C] px-4 py-2 rounded-xl text-white font-semibold w-full"
+                }
+              >
+                {link.linkText}
+              </Button>
+            </Link>
+          ))}
+        </div>
+
+        <SheetFooter className="flex justify-center items-center text-center">
+          <p>All rights reaserved @TaleSoul</p>
+        </SheetFooter>
+      </SheetContent>
+    </Sheet>
+  );
+}
+
+
+8917592361
+// jio  
